4f89ec68430bf5cc89d0f62b02f9b623315db2bd
amina01hu <amina01hu>
2022-04-16 01:23:51 -0500

    7th commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,15 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { PoststableComponent } from './poststable/poststable.component';
 import { NewpostComponent } from './newpost/newpost.component';
 import { EditpostComponent } from './editpost/editpost.component';
+import { PostcardComponent } from './postcard/postcard.component';
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'blog', component: BlogComponent},
   {path: 'post', component:PostComponent},
   {path: 'admin', component: PoststableComponent},
   {path: 'admin/newPost', component:NewpostComponent},
+  {path: 'post/:id', component: PostComponent},
+  {path: 'admin/post/:id', component: EditpostComponent},
   {path: '', component: HomeComponent},
   {path: '**', component: PagenotfoundComponent}
 ];
